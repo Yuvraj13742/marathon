fa911126332f96ca4e538ab075d2161dbcb2264e
yuvraj13742 <13742shyuvraj@gmail.com>
2025-11-08 19:01:31 +0530

    initial commit

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -3,7 +3,9 @@ import { hc } from "hono/client";
 
 
 
-const BASE_URL =  "https://marathon-16-website.vercel.app/"
+const BASE_URL = process.env.NODE_ENV === "production"
+    ? "https://marathon-16-website.vercel.app"
+    : "http://localhost:3000";
 const client = hc<ApiType>(BASE_URL);
 
 
